3c6daf6f9a2c778074c256531bbbd6f5f2bb50a3
Philip Gregorius <philip@reges.se>
2018-03-21 17:45:30 +0100

    change stack print

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 module.exports = printStack => (err, req, res, next) => {
     if (err.isJoi) err.status = 422;
+    const stack = err.stack.split('\n    ');
 
     const response = {
         message: (err.response && err.response.data) || err.message,
-        status: err.status || (err.response && err.response.status) || 500,
-        stack: err.stack
+        status: err.status || (err.response && err.response.status) || 500
     };
 
-    return res.status(response.status).send(printStack ? response : response.message);
+    if(printStack) {
+        response.stack = stack
+    }
+
+    return res.status(response.status).send(response);
 };
