e89c007663ac1cc0af7e0b83372f220b87fbefda
Philip Gregorius <reges@Philips-MacBook-Pro-2.local>
2019-05-14 17:04:10 +0200

    update for celebrate 10

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const { isCelebrate } = require('celebrate');
+
 module.exports = printStack => (err, req, res, next) => {
-    if (err.isJoi) err.status = 422;
-    const stack = err.stack.split('\n    ');
-
-    const response = {
-        message: (err.response && err.response.data) || err.message,
-        status: err.status || (err.response && err.response.status) || 500
-    };
-
-    if(printStack) {
-        response.stack = stack
+    if (isCelebrate(err)) {
+      err = err.joi;
+      err.status = 422;
     }
-
+  
+    const stack = err.stack.split('\n    ');
+  
+    const response = {
+      message: (err.response && err.response.data) || err.message,
+      status: err.status || (err.response && err.response.status) || 500,
+    };
+  
+    if (printStack) {
+      response.stack = stack;
+    }
+  
     return res.status(response.status).send(response);
-};
+  };
+  
